63b1e402c112dd0efd9898f4db0acb99b6bf6cf3
iotuz <Theismillinger@gmail.com>
2025-04-30 12:07:51 +0200

    Models Getters, Setters, constructor, tostring

--- src/Models/StockMarket.java
+++ src/Models/StockMarket.java
@@ -1,11 +1,128 @@
 package Models;
 
+import java.time.LocalDate;
+
 public class StockMarket {
 
     //TODO
+
+    private String ticker;
+    private String name;
+    private String sector;
+    private double price;
+    private Currency currency;
+    private String rating;
+    private double dividend_yield;
+    private String market;
+    private LocalDate LastUpdate;
+
+    public StockMarket(String ticker, String name, String sector, double price, Currency currency, String rating, double dividend_yield, String market, LocalDate lastUpdate) {
+        this.ticker = ticker;
+        this.name = name;
+        this.sector = sector;
+        this.price = price;
+        this.currency = currency;
+        this.rating = rating;
+        this.dividend_yield = dividend_yield;
+        this.market = market;
+        LastUpdate = lastUpdate;
+    }
+
+    @Override
+    public String toString() {
+        return "StockMarket{" +
+                "ticker='" + ticker + '\'' +
+                ", name='" + name + '\'' +
+                ", sector='" + sector + '\'' +
+                ", price=" + price +
+                ", currency=" + currency +
+                ", rating='" + rating + '\'' +
+                ", dividend_yield=" + dividend_yield +
+                ", market='" + market + '\'' +
+                ", LastUpdate=" + LastUpdate +
+                '}';
+    }
+
     /// Attributes
 
-    /// Constructor
+
+
+
+
+
+
+    public String getTicker() {
+        return ticker;
+    }
+
+    public void setTicker(String ticker) {
+        this.ticker = ticker;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSector() {
+        return sector;
+    }
+
+    public void setSector(String sector) {
+        this.sector = sector;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public Currency getCurrency() {
+        return currency;
+    }
+
+    public void setCurrency(Currency currency) {
+        this.currency = currency;
+    }
+
+    public String getRating() {
+        return rating;
+    }
+
+    public void setRating(String rating) {
+        this.rating = rating;
+    }
+
+    public double getDividend_yield() {
+        return dividend_yield;
+    }
+
+    public void setDividend_yield(double dividend_yield) {
+        this.dividend_yield = dividend_yield;
+    }
+
+    public String getMarket() {
+        return market;
+    }
+
+    public void setMarket(String market) {
+        this.market = market;
+    }
+
+    public LocalDate getLastUpdate() {
+        return LastUpdate;
+    }
+
+    public void setLastUpdate(LocalDate lastUpdate) {
+        LastUpdate = lastUpdate;
+    }
+/// Constructor
 
 
     /// Getters
--- src/Models/User.java
+++ src/Models/User.java
@@ -1,8 +1,101 @@
 package Models;
 
+import java.time.LocalDate;
+
 public class User {
 
+
+    private int userID;
+    private String fullName;
+    private String email;
+    private LocalDate birthDate;
+    private double initCash;
+    private LocalDate createdAt;
+    private LocalDate updateded;
     //TODO
+
+
+    public User(int userID, String fullName, String email, LocalDate birthDate, double initCash, LocalDate createdAt, LocalDate updateded) {
+        this.userID = userID;
+        this.fullName = fullName;
+        this.email = email;
+        this.birthDate = birthDate;
+        this.initCash = initCash;
+        this.createdAt = createdAt;
+        this.updateded = updateded;
+    }
+
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "userID=" + userID +
+                ", fullName='" + fullName + '\'' +
+                ", email='" + email + '\'' +
+                ", birthDate=" + birthDate +
+                ", initCash=" + initCash +
+                ", createdAt=" + createdAt +
+                ", updateded=" + updateded +
+                '}';
+    }
+
+    public int getUserID() {
+        return userID;
+    }
+
+    public void setUserID(int userID) {
+        this.userID = userID;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(LocalDate birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public double getInitCash() {
+        return initCash;
+    }
+
+    public void setInitCash(double initCash) {
+        this.initCash = initCash;
+    }
+
+    public LocalDate getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDate createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public LocalDate getUpdateded() {
+        return updateded;
+    }
+
+    public void setUpdateded(LocalDate updateded) {
+        this.updateded = updateded;
+    }
+
+
     /// Attributes
 
     /// Constructor
