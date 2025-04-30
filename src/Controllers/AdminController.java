52761a49ef5943cf92bc3ccb76540007032a8397
nikholt1 <140625263+nikholt1@users.noreply.github.com>
2025-04-30 10:25:03 +0200

    TODO add on Controllers

--- src/Controllers/AdminController.java
+++ src/Controllers/AdminController.java
@@ -1,4 +1,16 @@
 package Controllers;
 
 public class AdminController {
+
+
+    //TODO
+    /// add attributes, Constructor getters and setters
+
+    /// start method()
+    /// implement navigation functionality between service class and UI
+    /// switch call methods from services.
+
+
+    ///
+
 }
--- src/Controllers/UserController.java
+++ src/Controllers/UserController.java
@@ -1,4 +1,13 @@
 package Controllers;
 
 public class UserController {
+
+    //TODO
+    /// add attributes, Constructor getters and setters
+
+    /// start method()
+    /// implement navigation functionality between service class and UI
+    /// switch call methods from services.
+
+
 }
