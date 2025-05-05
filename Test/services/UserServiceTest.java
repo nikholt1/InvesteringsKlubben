06b3747928d853764377db0aa38582e1acb13214
nikholt1 <140625263+nikholt1@users.noreply.github.com>
2025-05-05 13:31:41 +0200

    Resumed UserService functionality Implementation
    - refactored UserRepository code to correctly handle file updates and writes.
    
    - implemented functionality in userService to
    + find userDate
    +updateUserCashValue
    + writeNewUser
    + printAllUsers
    + getUserID from email

--- Test/services/UserServiceTest.java
+++ Test/services/UserServiceTest.java
@@ -6,6 +6,9 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import repositories.UserRepository;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class UserServiceTest {
@@ -26,22 +29,52 @@ class UserServiceTest {
     @Test
     void findUserData_test_correct_user_found_return_true() {
         //1;Maria Jensen;maria.jensen@email.com;12-04-1990;100000;01-03-2025;01-03-2025
-
         //TODO
         // User user = user user i assertTrue fra return type User
+        assertEquals("User{userID=1, fullName='Maria Jensen', email='maria.jensen@email.com', birthDate=1990-04-12, initCash=100000.0, createdAt=2025-03-01, updateded=2025-03-01}", u.findUserData("maria.jensen@email.com").toString());
 
     }
     @Test
     void findUserData_test_email_not_in_repository_return_true() {
         // TODO
         // user user = user user i assert>True fra return type user stemmer ikke overens med method return value
+        assertEquals(null, u.findUserData("hasbulla@dagestan.rus"));
+
     }
+    //test updateUserCashValue()
+    @Test
+    void updateUserCashValue_for_existing_user_return_true() {
+        assertTrue(u.updateUserCashValue("hasbulla@donesk.ru", 10_000));
+    }
+    @Test
+    void updateUserCashValue_for_non_existing_user_return_false() {
+        assertFalse(u.updateUserCashValue("zlatanibrahimovic@sweden.se", 10_000));
+    }
+
+    //test writeNewUser()
+    @Test
+    void writeNewUser_append_new_user_with_correct_minimal_initCash_return_true() {
+        assertTrue(u.writeNewUser("King of Mauretania", "atlas@northAfrica.ly", 10_000, "04-08-1997"));
+    }
+
+    @Test
+    void writeNewUser_append_new_user_with_incorrect_minimal_initCash_return_false() {
+        assertFalse(u.writeNewUser("King of Mauretania", "atlas@northAfrica.ly", 9_000, "04-08-1997"));
+    }
+
 
     // test getUserID
     @Test
     void getUserID_test_correct_id_return_true() {
+
         assertTrue(u.getUserID("maria.jensen@email.com") == 1);
     }
+
+    @Test
+    void getUserID_test_userNotFound_return_true() {
+        assertTrue(u.getUserID("hasbulla@dagestan.rus") == -1);
+    }
+
 
 
 
--- src/repositories/UserRepository.java
+++ src/repositories/UserRepository.java
@@ -14,7 +14,7 @@ import java.util.Scanner;
 
 public class UserRepository {
 
-    private List<User> users;
+    private  List<User> users;
     private final String PATH = "src/repositories/users.csv";
     private static DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
     //TODO
@@ -27,6 +27,7 @@ public class UserRepository {
 
     // readFile()
     public void readFile() {
+        users.clear();
         try {
             Scanner reader = new Scanner(new File(PATH));
             while (reader.hasNextLine()) {
@@ -49,40 +50,85 @@ public class UserRepository {
         }
     }
     // writeToFile()
-    public void writeToFile(String name, String email, LocalDate birthDate) {
+    public void writeNewUserToFile(String name, String email, double initCash, String birthDate) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+
         try {
-            Writer writer = new FileWriter(new File(PATH), true);
-            readFile();
-            int newID = 0;
+            Writer writer = new FileWriter(new File(PATH), true); // append mode
+            int ID = 0;
             for (User user : users) {
-                newID++;
+                ID++;
             }
-            LocalDate updated = LocalDate.now();
+            int newID = ID + 1;
+
             LocalDate created = LocalDate.now();
-            String formatted = name + ";" + email + ";" + "100_000" + ";" + created + ";" + updated;
-            writer.write(formatted);
+            LocalDate updated = LocalDate.now();
+            LocalDate parsedBirthDate = LocalDate.parse(birthDate, formatter);
+
+            String formattedBirthDate = parsedBirthDate.format(formatter);
+            String formattedCreated = created.format(formatter);
+            String formattedUpdated = updated.format(formatter);
+
+            String formattedLine = newID + ";" + name + ";" + email + ";" +
+                    formattedBirthDate + ";" + initCash + ";" +
+                    formattedCreated + ";" + formattedUpdated + "\n";
+            writer.write(formattedLine);
             writer.close();
+            readFile();
 
         } catch (IOException e) {
             System.out.println("File not found");
             e.printStackTrace();
         }
     }
-    //getUser()
-    public User findUserMyEmail(String email) {
-        User result = null;
-        for (User user : users) {
-            if(user.getEmail().equals(email)) {
-                result = user;
 
+
+    public void updateUserCashData(int userID, int value) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+
+        for (User user : users) {
+            if (user.getUserID() == userID) {
+                user.setInitCash(value);
+                user.setUpdateded(LocalDate.now());
             }
         }
-        if (result == null) {
-            System.out.println("Systemet kunne ikke finde den indtastede bruger.");
+        try {
+            FileWriter writer = new FileWriter(PATH);
 
+            for (User user : users) {
+                String formatted = user.getUserID() + ";" +
+                        user.getFullName() + ";" +
+                        user.getEmail() + ";" +
+                        user.getBirthDate().format(formatter) + ";" +
+                        user.getInitCash() + ";" +
+                        user.getCreatedAt().format(formatter) + ";" +
+                        user.getUpdateded().format(formatter) + "\n";
+                writer.write(formatted);
+            }
+
+            writer.close();
+            readFile();
+        } catch (IOException e) {
+            System.out.println("File not found or could not be written to");
+            e.printStackTrace();
         }
-        return result;
     }
+
+    //getUser()
+//    public User findUserMyEmail(String email) {
+//        User result = null;
+//        for (User user : users) {
+//            if(user.getEmail().equals(email)) {
+//                result = user;
+//
+//            }
+//        }
+//        if (result == null) {
+//            System.out.println("Systemet kunne ikke finde den indtastede bruger.");
+//
+//        }
+//        return result;
+//    }
 
 
 
