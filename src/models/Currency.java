844f2eb18b6bceac526c5f3deedb46521a34bde3
Manden9753 <andr9753@gmail.com>
2025-05-05 09:22:13 +0200

    Currency repo and Currency models change

--- src/models/Currency.java
+++ src/models/Currency.java
@@ -6,150 +6,63 @@ public class Currency {
 
     //TODO
 
-    private double DKKToDKK;
-    private double EURToDKK;
-    private double USDToDKK;
-    private double SEKToDKK;
-    private double NOKToDKK;
-    private double GBPToDKK;
-    private double JPYToDKK;
-    private double CHFToDKK;
-    private double AUDToDKK;
-    private double CADToDKK;
-    private String base_Currency;
-    private LocalDate LastUpdate;
 
-    public Currency(double DKKToDKK, double EURToDKK, double USDToDKK, double SEKToDKK, double NOKToDKK, double GBPToDKK, double CHFToDKK, double JPYToDKK, double AUDToDKK, double CADToDKK, String base_Currency, LocalDate lastUpdate) {
-        this.DKKToDKK = DKKToDKK;
-        this.EURToDKK = EURToDKK;
-        this.USDToDKK = USDToDKK;
-        this.SEKToDKK = SEKToDKK;
-        this.NOKToDKK = NOKToDKK;
-        this.GBPToDKK = GBPToDKK;
-        this.CHFToDKK = CHFToDKK;
-        this.JPYToDKK = JPYToDKK;
-        this.AUDToDKK = AUDToDKK;
-        this.CADToDKK = CADToDKK;
-        this.base_Currency = base_Currency;
-        LastUpdate = lastUpdate;
+    private String base_currency;
+    private String quote_Currency;
+    private double rate;
+    private LocalDate Last_update;
+
+    public Currency(String base_currency, String quote_Currency, double rate, LocalDate last_update) {
+        this.base_currency = base_currency;
+        this.quote_Currency = quote_Currency;
+        this.rate = rate;
+        Last_update = last_update;
     }
 
     @Override
     public String toString() {
         return "Currency{" +
-                "DKKToDKK=" + DKKToDKK +
-                ", EURToDKK=" + EURToDKK +
-                ", USDToDKK=" + USDToDKK +
-                ", SEKToDKK=" + SEKToDKK +
-                ", NOKToDKK=" + NOKToDKK +
-                ", GBPToDKK=" + GBPToDKK +
-                ", JPYToDKK=" + JPYToDKK +
-                ", CHFToDKK=" + CHFToDKK +
-                ", AUDToDKK=" + AUDToDKK +
-                ", CADToDKK=" + CADToDKK +
-                ", base_Currency='" + base_Currency + '\'' +
-                ", LastUpdate=" + LastUpdate +
+                "base_currency='" + base_currency + '\'' +
+                ", quote_Currency='" + quote_Currency + '\'' +
+                ", rate=" + rate +
+                ", Last_update=" + Last_update +
                 '}';
     }
 
-    /// Attributes
-
-
-    public double getDKKToDKK() {
-        return DKKToDKK;
+    public String getBase_currency() {
+        return base_currency;
     }
 
-    public void setDKKToDKK(double DKKToDKK) {
-        this.DKKToDKK = DKKToDKK;
+    public void setBase_currency(String base_currency) {
+        this.base_currency = base_currency;
     }
 
-    public double getEURToDKK() {
-        return EURToDKK;
+    public String getQuote_Currency() {
+        return quote_Currency;
     }
 
-    public void setEURToDKK(double EURToDKK) {
-        this.EURToDKK = EURToDKK;
+    public void setQuote_Currency(String quote_Currency) {
+        this.quote_Currency = quote_Currency;
     }
 
-    public double getSEKToDKK() {
-        return SEKToDKK;
+    public double getRate() {
+        return rate;
     }
 
-    public void setSEKToDKK(double SEKToDKK) {
-        this.SEKToDKK = SEKToDKK;
+    public void setRate(double rate) {
+        this.rate = rate;
     }
 
-    public double getUSDToDKK() {
-        return USDToDKK;
+    public LocalDate getLast_update() {
+        return Last_update;
     }
 
-    public void setUSDToDKK(double USDToDKK) {
-        this.USDToDKK = USDToDKK;
+    public void setLast_update(LocalDate last_update) {
+        Last_update = last_update;
     }
+}
 
-    public double getNOKToDKK() {
-        return NOKToDKK;
-    }
 
-    public void setNOKToDKK(double NOKToDKK) {
-        this.NOKToDKK = NOKToDKK;
-    }
-
-    public double getGBPToDKK() {
-        return GBPToDKK;
-    }
-
-    public void setGBPToDKK(double GBPToDKK) {
-        this.GBPToDKK = GBPToDKK;
-    }
-
-    public double getJPYToDKK() {
-        return JPYToDKK;
-    }
-
-    public void setJPYToDKK(double JPYToDKK) {
-        this.JPYToDKK = JPYToDKK;
-    }
-
-    public double getCHFToDKK() {
-        return CHFToDKK;
-    }
-
-    public void setCHFToDKK(double CHFToDKK) {
-        this.CHFToDKK = CHFToDKK;
-    }
-
-    public double getAUDToDKK() {
-        return AUDToDKK;
-    }
-
-    public void setAUDToDKK(double AUDToDKK) {
-        this.AUDToDKK = AUDToDKK;
-    }
-
-    public double getCADToDKK() {
-        return CADToDKK;
-    }
-
-    public void setCADToDKK(double CADToDKK) {
-        this.CADToDKK = CADToDKK;
-    }
-
-    public String getBase_Currency() {
-        return base_Currency;
-    }
-
-    public void setBase_Currency(String base_Currency) {
-        this.base_Currency = base_Currency;
-    }
-
-    public LocalDate getLastUpdate() {
-        return LastUpdate;
-    }
-
-    public void setLastUpdate(LocalDate lastUpdate) {
-        LastUpdate = lastUpdate;
-    }
 /// Constructor
 
 
@@ -161,4 +74,4 @@ public class Currency {
 
     /// toString
 
-}
+
