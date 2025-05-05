67c936d42afa502ae17b4fe615d9c9ff83525fb9
iotuz <Theismillinger@gmail.com>
2025-05-05 10:59:20 +0200

    Models Getters, Setters, constructor, tostring

--- src/services/StockMarketService.java
+++ src/services/StockMarketService.java
@@ -1,19 +1,61 @@
 package services;
 
+import models.StockMarket;
+import repositories.StockMarketRepository;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class StockMarketService {
 
+    private StockMarketRepository stockMarketRepository;
+    private List<StockMarket> stockMarkets;
 
     //TODO
-
-    //List<StockMarked>
+    public StockMarketService(StockMarketRepository stockMarketRepository) {
+        this.stockMarketRepository = stockMarketRepository;
+        stockMarketRepository.readList();
+        stockMarkets = stockMarketRepository.getStockMarket();
+    }
 
 
     // getStockMarket()
+    public StockMarketRepository getStockMarketRepository() {
+        return stockMarketRepository;
+    }
 
     // getTicker()
+    public String getTicker(String ticker) {
+        String foundTicker = " ";
+        for (StockMarket stockMarket : stockMarkets) {
+            if (stockMarket.getTicker().equals(ticker)) {
+                foundTicker = stockMarket.getTicker();
+                return foundTicker;
+            }
+            else return "Ticker not found";
+        }
+        return null;
+    }
 
     // getPrice()
+    public double getPrice(Double price) {
+        double foundPrice = 0;
+        for (StockMarket stockMarket : stockMarkets) {
+            if (stockMarket.getPrice() == price) {
+                foundPrice = stockMarket.getPrice();
+            }
+            else return foundPrice;
+        }
+        return foundPrice;
+    }
 
     // getCurrency()
+    public String getCurrency() {
+        String foundCurrency = " ";
+        for (StockMarket stockMarket : stockMarkets) {
+            foundCurrency = stockMarket.getCurrency();
+        }
+        return foundCurrency;
+    }
 
 }
