53aa9821a267c3247cd875b3eeaa8062aa50fcb8
nikholt1 <140625263+nikholt1@users.noreply.github.com>
2025-05-06 11:42:29 +0200

    Implementation of camparators for userService sorting.
    - mkdir comparators directory
    - sortalphabetical with tests
    - sortNumerical
    - refactoreduserModel to take String in parameters
    + automatically update to LocalDateTime

--- Test/services/UserServiceTest.java
+++ Test/services/UserServiceTest.java
@@ -8,6 +8,7 @@ import repositories.UserRepository;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -74,6 +75,102 @@ class UserServiceTest {
     void getUserID_test_userNotFound_return_true() {
         assertTrue(u.getUserID("hasbulla@dagestan.rus") == -1);
     }
+
+
+    // ComparatorTests
+    //sort usersByNameMethod:
+    @Test
+    void sortUsersByName_functional_Alphabetical_sort_return_true() {
+        assertTrue(u.sortUsersByName());
+    }
+    @Test
+    void sortUsersByName_Correct_Alphabetical_sort_return_return_true() {
+        // skal være string pga:
+        /// expected: models.User@77e9807f<User{userID=5, fullName='Anders Holm', email='anders.holm@email.com', birthDate=1988-05-22, initCash=100000.0, createdAt=2025-03-05, updateded=2025-03-05}> but was: models.User@448ff1a8<User{userID=5, fullName='Anders Holm', email='anders.holm@email.com', birthDate=1988-05-22, initCash=100000.0, createdAt=2025-03-05, updateded=2025-03-05}
+        u.sortUsersByName();
+        User user1 = new User(
+                5,
+                "Anders Holm",
+                "anders.holm@email.com",
+                "22-05-1988",
+                100000.0,
+                "05-03-2025",
+                "05-03-2025"
+        );
+        List<User> users = u.getAllUsers();
+        User actualUser = null;
+        actualUser = users.get(0);
+        System.out.println(user1);
+        System.out.println(actualUser);
+        assertEquals(actualUser.toString(), user1.toString());
+    }
+
+    @Test
+    void sortUsersByName_Incorrect_Alphabetical_sort_return_return_false() {
+        u.sortUsersByName();
+        User comparableUser = new User(
+                5,
+                "Zanders Holm",
+                "anders.holm@email.com",
+                "22-05-1988",
+                100000.0,
+                "05-03-2025",
+                "05-03-2025"
+        );
+        List<User> users = u.getAllUsers();
+        User actualUser = users.get(0);
+        System.out.println(comparableUser);
+        System.out.println(actualUser);
+        assertFalse(actualUser.toString().equals(comparableUser.toString()));
+    }
+
+    //SortUsersByCash method:
+
+    @Test
+    void sortUsersByCash_functional_CashSort_return_true() {
+        assertTrue(u.sortUsersByCash());
+    }
+
+    @Test
+    void sortUsersByCash_Correct_sort_return_highest_to_lowest_return_true() {
+        u.sortUsersByCash();
+        User comparableUser = new User(
+                11,
+                "hasbulla",
+                "hasbulla@outsideDagestan.ru",
+                "17-06-1993",
+                100001.0,
+                "07-03-2025",
+                "05-05-2025"
+        );
+        List<User> users = u.getAllUsers();
+        User actualUser = users.get(0);
+        System.out.println(comparableUser);
+        System.out.println(actualUser);
+        assertEquals(actualUser.toString(), comparableUser.toString());
+    }
+    @Test
+    void sortUsersByCash_incorrect_sort_return_highest_to_lowest_test_reverse_output_return_false() {
+        u.sortUsersByCash();
+        User comparableUser = new User(
+                1,
+                "Maria Jensen",
+                "maria.jensen@email.com",
+                "12-04-1990",
+                100000.0,
+                "01-03-2025",
+                "01-03-2025"
+        );
+
+        List<User> users = u.getAllUsers();
+        User actualUser = users.get(0);
+        System.out.println(comparableUser);
+        System.out.println(actualUser);
+        assertFalse(actualUser.toString().equals(comparableUser.toString()));
+    }
+
+
+
 
 
 
---
+++ src/comparators/ComparatoruserSortByCash.java
@@ -0,0 +1,13 @@
+package comparators;
+
+import models.User;
+
+import java.util.Comparator;
+
+public class ComparatoruserSortByCash implements Comparator<User> {
+
+    @Override
+    public int compare(User u1, User u2) {
+        return Double.compare(u2.getInitCash(), u1.getInitCash());
+    }
+}
